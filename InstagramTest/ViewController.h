a170cc5608a1c6ac7eb727f7674286647c155c02
張揚法 <nttu94413@yahoo.com.tw>
2018-03-22 14:04:36 +0800

    Initial Commit

---
+++ InstagramTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramTest
+//
+//  Created by 張揚法 on 2018/3/22.
+//  Copyright © 2018年 張揚法. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstagramTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstagramTest
+//
+//  Created by 張揚法 on 2018/3/22.
+//  Copyright © 2018年 張揚法. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
